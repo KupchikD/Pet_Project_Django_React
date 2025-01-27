f4e8f54c5ed4bdfcf3ca3d02d859d35bbea26733
Kupchyk Dmyto <kupchik03@gmail.com>
2025-01-27 23:17:08 +0100

    Styles for product page

--- frontend/src/product_card.jsx
+++ frontend/src/product_card.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import './product_card.css'; // Стилевой файл для компонента
+import './product_card.css';
 
 const ProductPage = () => {
-  const { productId } = useParams();  // Получаем id товара из URL
+  const { productId } = useParams();
   const [product, setProduct] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -25,17 +25,30 @@ const ProductPage = () => {
   if (error) return <div>Ошибка: {error.message}</div>;
 
   return (
-    <div>
-      {product && (
-        <div className="product-card">
-          <h2>{product.name}</h2>
+
+  <div className="container">
+  {product && (
+    <div className="product-card">
+      <div className="product-image">
+        <div className="image-placeholder">
           <img src={`http://127.0.0.1:8000${product.image}`} alt={product.name} />
-          <p>Цена: {product.price}</p>
-          <p>Статус: {product.status}</p>
-          <p>{product.description}</p>
         </div>
-      )}
+      </div>
+      <div className="product-info">
+        <h1 className="product-title">{product.name}</h1>
+        <p className="product-meta">{product.price}$</p>
+        <p className="product-meta">27.01.2025</p>
+        <p className="product-description">{product.description}</p>
+        <div class="btn-container">
+        <button className="btn reserve">Reserve</button>
+        <button className="btn view">View on "Amazon"</button>
+        </div>
+      </div>
     </div>
+  )}
+  <p className="note">Giving this as a gift? Reserve it to avoid duplicate gifts.</p>
+</div>
+
   );
 };
 
